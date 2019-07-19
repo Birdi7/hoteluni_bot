4b5f30854c559bb623ca195c0a51fb14ac3f1996
birdi <egor.birdi7@gmail.com>
2019-07-19 13:51:57 +0300

    Remove credentials data

--- core/configs/telegram.py
+++ core/configs/telegram.py
@@ -1,5 +1,5 @@
 # Here is your telegram config data such as bot token, admin id's etc.
 import os
 
-BOT_TOKEN = os.getenv('BOT_TOKEN', '834469947:AAFiTmj_5Hq5Vm3m9DmSN7ydEJx9HSkMpoM')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_IDS = []
