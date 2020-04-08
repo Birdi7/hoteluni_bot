3be6fae63aa199d17ab185666f07bf52e3272047
Egor <egor.birdi7@gmail.com>
2020-04-08 10:30:45 +0300

    fix again

--- core/handlers.py
+++ core/handlers.py
@@ -467,7 +467,7 @@ async def mailing_everyone_handler(msg: types.Message, state: FSMContext):
 
 
 async def send_to_everyone(txt):
-    for u in user_model.User.find_many({}):
+    for u in user_model.User.find({}):
         try:
             await bot.send_message(u.chat_id, txt)
         except TelegramAPIError:
